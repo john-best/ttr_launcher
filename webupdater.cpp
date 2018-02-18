1e2eaa374da8bf1ce90373d0285cb916f310e09b
John <pwnerjohn@gmail.com>
2018-02-17 18:09:53 -0800

    starting work for downloading files

--- webupdater.cpp
+++ webupdater.cpp
@@ -1,5 +1,6 @@
 #include "webupdater.h"
 #include <QDebug>
+#include <QFileInfo>
 
 WebUpdater::WebUpdater(){
     networkManager = new QNetworkAccessManager(this);
@@ -43,12 +44,46 @@ void WebUpdater::handle_network_response(QNetworkReply *reply) {
         // TODO: there's probably a more elegant way of handling this-- need to figure out what the response is...
         if (json_object.contains("title")) {
             QString str = QString::fromStdString("<p>") + json_object["date"].toString() + QString::fromStdString("</p>") + json_object["body"].toString();
-            qDebug() << "update web" << str;
             emit update_news_request(true, str.toStdString());
 
         } else {
             qDebug() << "manifest size: " << json_object.size();
+
+            for (QString filename : json_object.keys()) {
+                QString hash = json_object[filename].toObject()["hash"].toString();
+
+
+                QJsonArray only_array = json_object[filename].toObject()["only"].toArray();
+                bool is_win_32 = only_array.contains(QJsonValue(QString::fromStdString("win32")));
+
+                // is this going to be a windows only launcher?
+                if (!is_win_32) {
+                    qDebug() << filename << " is not required for win32. skipping...";
+                    continue;
+                }
+
+                if (!file_up_to_date(filename.toStdString(), hash.toStdString())) {
+                    // TODO: redownload the file, patches in the future i guess...
+                    qDebug() << filename << " not up to date!";
+                } else {
+                    qDebug() << filename << " up to date.";
+                }
+            }
         }
     }
     reply->deleteLater();
+}
+
+bool WebUpdater::file_up_to_date(std::string path, std::string dl_hash) {
+    QFileInfo file(QString::fromStdString(path));
+    bool exists = file.exists() && file.isFile();
+
+    if (!exists) return false;
+
+    // waiting on figuring out what hash and compHash are!
+    return dl_hash == get_bz2_hash(path);
+}
+
+std::string WebUpdater::get_bz2_hash(std::string path) {
+    return "TODO";
 }
--- webupdater.h
+++ webupdater.h
@@ -19,6 +19,8 @@ private slots:
     void handle_network_response(QNetworkReply *reply);
 private:
     QNetworkAccessManager *networkManager;
+    bool file_up_to_date(std::string path, std::string dl_hash);
+    std::string get_bz2_hash(std::string path);
 };
 
 #endif // WEBUPDATER_H
