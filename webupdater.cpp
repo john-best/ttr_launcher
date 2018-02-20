20feb05666084340dee60e3078086103eb5b5168
John <pwnerjohn@gmail.com>
2018-02-19 23:15:04 -0800

    threading for webupdater-- awful naming conventions included

--- webupdater.cpp
+++ webupdater.cpp
@@ -1,14 +1,28 @@
 #include "webupdater.h"
 #include <QDebug>
 #include <QFileInfo>
+#include <QMetaType>
 
-WebUpdater::WebUpdater(){
+WebUpdater::WebUpdater() {
     networkManager = new QNetworkAccessManager(this);
     connect(networkManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(handle_network_response(QNetworkReply*)));
 
+
+    qRegisterMetaType<std::string>("std::string");
+    qRegisterMetaType<std::vector<std::pair<std::string,std::string>>>("std::vector<std::pair<std::string,std::string>>");
+    WebUpdaterWorker *worker = new WebUpdaterWorker;
+    worker->moveToThread(&workerThread);
+    connect(&workerThread, SIGNAL(finished()), worker, SLOT(deleteLater()));
+    connect(this, SIGNAL(handle_manifest(QByteArray)), worker, SLOT(handle_manifest_worker(QByteArray)));
+    connect(worker, SIGNAL(update_download_request_worker(double,std::string)), this, SLOT(handle_download_request(double,std::string)));
+    connect(worker, SIGNAL(resultReady(std::vector<std::pair<std::string,std::string>>)), this, SLOT(handle_finished_manifest_request(std::vector<std::pair<std::string,std::string>>)));
+    workerThread.start();
 }
+
 WebUpdater::~WebUpdater(){
     delete networkManager;
+    workerThread.quit();
+    workerThread.wait();
 }
 
 void WebUpdater::get_news() {
@@ -29,7 +43,6 @@ void WebUpdater::handle_network_response(QNetworkReply *reply) {
         emit update_news_request(false, reply->errorString().toStdString());
     } else {
         QByteArray data = reply->readAll();
-
         QJsonObject json_object = QJsonDocument::fromJson(data).object();
 
         // "title" is only in news -- since we only update manifest and news, don't think we need to change much for now.
@@ -38,45 +51,7 @@ void WebUpdater::handle_network_response(QNetworkReply *reply) {
             emit update_news_request(true, str.toStdString());
 
         } else {
-
-            // pair<filename, dl in bz2>
-            std::vector<std::pair<std::string, std::string>> needs_dl;
-
-            double size = json_object.size();
-            double counter = 0.0;
-
-            for (QString filename : json_object.keys()) {
-                QString hash = json_object[filename].toObject()["hash"].toString();
-
-                QJsonArray only_array = json_object[filename].toObject()["only"].toArray();
-                bool is_win_32 = only_array.contains(QJsonValue(QString::fromStdString("win32")));
-
-                // is this going to be a windows only launcher?
-                if (!is_win_32) {
-                    qDebug() << filename << "is not required for win32. skipping...";
-                    continue;
-                }
-
-                if (!file_up_to_date(filename.toStdString(), hash.toStdString())) {
-
-                    std::string s = (filename + " not up to date.").toStdString();
-                    emit update_download_request((counter/size) * 100, s);
-
-                    qDebug() << filename << "not up to date!";
-                    QString dl_filename = json_object[filename].toObject()["dl"].toString();
-                    std::pair<std::string, std::string> dl_pair(filename.toStdString(), dl_filename.toStdString());
-                    needs_dl.push_back(dl_pair);
-                } else {
-                    std::string s = (filename + " up to date.").toStdString();
-
-                    // ok this probably isn't the best way to do this but it works for now.
-                    // TODO: what do we do when the files aren't up to date?
-                    emit update_download_request((++counter/size) * 100, s);
-                    qDebug() << filename << "up to date.";
-                }
-            }
-
-            emit download_files_request(needs_dl);
+            handle_manifest(data);
         }
     }
 
@@ -93,7 +68,56 @@ void WebUpdater::update_manifest() {
     networkManager->get(request);
 }
 
-bool WebUpdater::file_up_to_date(std::string path, std::string dl_hash) {
+void WebUpdater::handle_download_request(double progress, std::string text) {
+    emit update_download_request(progress, text);
+}
+
+void WebUpdater::handle_finished_manifest_request(std::vector<std::pair<std::string, std::string> > filenames_pair) {
+    emit download_files_request(filenames_pair);
+}
+
+// WebUpdateWorker methods
+void WebUpdaterWorker::handle_manifest_worker(QByteArray data) {
+
+    QJsonObject json_object = QJsonDocument::fromJson(data).object();
+    // pair<filename, dl in bz2>
+    std::vector<std::pair<std::string, std::string>> needs_dl;
+
+    double size = json_object.size();
+    double counter = 0.0;
+
+    for (QString filename : json_object.keys()) {
+        QString hash = json_object[filename].toObject()["hash"].toString();
+
+        QJsonArray only_array = json_object[filename].toObject()["only"].toArray();
+        bool is_win_32 = only_array.contains(QJsonValue(QString::fromStdString("win32")));
+
+        // is this going to be a windows only launcher?
+        if (!is_win_32) {
+            qDebug() << filename << "is not required for win32. skipping...";
+            continue;
+        }
+
+        if (!file_up_to_date(filename.toStdString(), hash.toStdString())) {
+            std::string s = (filename + " not up to date.").toStdString();
+            emit update_download_request_worker((counter/size) * 100, s);
+
+            qDebug() << filename << "not up to date!";
+            QString dl_filename = json_object[filename].toObject()["dl"].toString();
+            std::pair<std::string, std::string> dl_pair(filename.toStdString(), dl_filename.toStdString());
+            needs_dl.push_back(dl_pair);
+        } else {
+            std::string s = (filename + " up to date.").toStdString();
+            emit update_download_request_worker((++counter/size) * 100, s);
+            qDebug() << filename << "up to date.";
+        }
+    }
+
+    emit resultReady(needs_dl);
+}
+
+
+bool WebUpdaterWorker::file_up_to_date(std::string path, std::string dl_hash) {
     QFileInfo file(QString::fromStdString(path));
     bool exists = file.exists() && file.isFile();
 
@@ -110,6 +134,6 @@ bool WebUpdater::file_up_to_date(std::string path, std::string dl_hash) {
     }
 
     // bad fix, all updating stuff needs to run in a new thread or something
-    QCoreApplication::processEvents();
+    //QCoreApplication::processEvents();
     return dl_hash == file_hash;
 }
--- webupdater.h
+++ webupdater.h
@@ -3,9 +3,24 @@
 
 #include <QtNetwork>
 #include <string.h>
+
+class WebUpdaterWorker : public QObject {
+    Q_OBJECT
+    QThread workerThread;
+
+public slots:
+    void handle_manifest_worker(QByteArray data);
+signals:
+    void resultReady(std::vector<std::pair<std::string, std::string>> needs_dl);
+    void update_download_request_worker(double progress, std::string text);
+private:
+    bool file_up_to_date(std::string path, std::string dl_hash);
+};
+
 class WebUpdater : public QObject
 {
     Q_OBJECT
+    QThread workerThread;
 
 public:
     WebUpdater();
@@ -17,11 +32,13 @@ signals:
     void update_news_request(bool res, std::string in);
     void download_files_request(std::vector<std::pair<std::string, std::string>> filenames_pair);
     void update_download_request(double progress, std::string text);
+    void handle_manifest(QByteArray data);
 private slots:
     void handle_network_response(QNetworkReply *reply);
+    void handle_download_request(double progress, std::string text);
+    void handle_finished_manifest_request(std::vector<std::pair<std::string, std::string>> filenames_pair);
 private:
     QNetworkAccessManager *networkManager;
-    bool file_up_to_date(std::string path, std::string dl_hash);
 };
 
 #endif // WEBUPDATER_H
